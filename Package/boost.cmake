8e1a366e02f26e60597bad5c211c53a2c3c0b6b3
Brandon Foster <Brandon.Foster4525@outlook.com>
2025-03-29 14:03:09 -0400

    changed the build process

--- Package/boost.cmake
+++ Package/boost.cmake
@@ -1,5 +1,5 @@
 # Ensure we use static Boost libraries
-set(Boost_USE_STATIC_LIBS OFF)
+set(Boost_USE_STATIC_LIBS ON)
 set(Boost_USE_STATIC_RUNTIME OFF)  # Set to ON if needed
 set(Boost_USE_MULTITHREADED ON)
 
@@ -13,3 +13,10 @@ add_custom_target(
     COMMAND ${CMAKE_COMMAND}
         -DCopyPath="${CMAKE_BINARY_DIR}/$<CONFIG>/bin"
         -P "${CMAKE_CURRENT_LIST_DIR}/copy_dll.cmake")
+
+file(GLOB boost_include_folders "${CMAKE_CURRENT_LIST_DIR}/include/*")
+add_library(boost-all INTERFACE)
+add_dependencies(boost-all Copy_Boost_DLLs)
+target_include_directories(boost-all INTERFACE ${boost_include_folders})
+target_link_directories(boost-all INTERFACE "${CMAKE_CURRENT_LIST_DIR}/lib")
+add_library(boost::all ALIAS boost-all)
