fd73f41584c692bd7f78587bd6ae21c248b57c3e
Brandon Foster <Brandon.Foster4525@outlook.com>
2025-03-28 22:41:42 -0400

    add copying dll files

--- Package/boost.cmake
+++ Package/boost.cmake
@@ -6,3 +6,10 @@ set(Boost_USE_MULTITHREADED ON)
 # Specify Boost installation directory if not in a standard location
 set(BOOST_ROOT "${CMAKE_CURRENT_LIST_DIR}")
 set(Boost_NO_SYSTEM_PATHS ON)  # Ensures CMake looks only in BOOST_ROOT
+
+add_custom_target(
+    Copy_Boost_DLLs
+    COMMENT "Copying Boost DLLs"
+    COMMAND ${CMAKE_COMMAND}
+        -DCopyPath="${CMAKE_BINARY_DIR}/$<CONFIG>/bin"
+        -P "${CMAKE_CURRENT_LIST_DIR}/copy_dll.cmake")
