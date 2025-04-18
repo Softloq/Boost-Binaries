be2c83bcd7ccccb3cccc620ffa38f9916da10fa3
Brandon Foster <Brandon.Foster4525@outlook.com>
2025-04-18 19:18:33 -0400

    updated windows build support

--- msvc.bat
+++ msvc.bat
@@ -1,12 +1,12 @@
 @setlocal
 @for /F "delims== tokens=1,* eol=#" %%i in ('type .env') do @set %%i=%%~j
 
-cmake -S %~dp0Prerequisite -B %~dp0build/Prerequisite
+cmake -S %~dp0Prerequisite -B %~dp0build/Prerequisite -DBOOST_VERSION="%BOOST_VERSION%"
 cmake --build %~dp0build/Prerequisite
 
-cmake -S %~dp0Compile -B %~dp0build/Compile
+cmake -S %~dp0Compile -B %~dp0build/Compile -DBOOST_VERSION="%BOOST_VERSION%"
 cmake --build %~dp0build/Compile
 
-cmake -S %~dp0Package -B %~dp0build/Package
+cmake -S %~dp0Package -B %~dp0build/Package -DBOOST_VERSION="%BOOST_VERSION%" -DPACKAGE_NAME="boost-win64-msvc.zip"
 cmake --build %~dp0build/Package
 @endlocal
